7346237b059c3abce0ec9c9c90f43dbcb3bd4c9f
Lexy <aleksandra.vlasenk@gmail.com>
2024-08-13 12:30:35 +0300

    HW5

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,5 @@
-'use strict';
-const sortByDescendingFriendCount = users => 
-    users.sort((a, b) => b.friends.length - a.friends.length);
+"use strict";
+const sortByDescendingFriendCount = (users) => users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
